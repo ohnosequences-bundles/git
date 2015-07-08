3bdfa90e6aab10fa2ef86a00a57db32d3e157bd5
Marina Manrique <mmanrique@era7.com>
2015-07-08 17:18:12 +0200

    add git bundle

--- src/main/scala/git.scala
+++ src/main/scala/git.scala
@@ -5,9 +5,15 @@ import ohnosequences.statika._, bundles._, instructions._
 
 case object git extends Bundle() {
 
+
+
   def install: Results = {
-    // do someting here
-    success(s"${bundleName} is installed")
+    Seq("yum", "install", "-y", "git") ->-
+		success(s"${bundleName} is installed")
+
   }
+
+  def clone(repo: String, dir: String = ""): Results =
+  	Seq("git", "clone", repo) ++ (if (dir.isEmpty) Seq() else Seq(dir))
 
 }
